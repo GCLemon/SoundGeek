2cac2fc26188b98a0bd1c14e1820e22ae19e7523
GCLemon <green.c.lemon@gmail.com>
2023-04-16 00:46:19 +0900

    ノーツの見た目を作ってみた

---
+++ Assets/Game/Scripts/ScorePlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePlayer : MonoBehaviour
+{
+    [SerializeField]
+    AnimationCurve _LeftCurve;
+
+    [SerializeField]
+    AnimationCurve _RightCurve;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
